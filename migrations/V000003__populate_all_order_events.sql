c893cc03520cc0b0ed7daf07b23d706fe7fa3f66
Zara Lim <zlim@confluent.io>
2021-05-25 13:34:05 -0700

    add column name

--- migrations/V000003__populate_all_order_events.sql
+++ migrations/V000003__populate_all_order_events.sql
@@ -1,7 +1,7 @@
 INSERT INTO ALL_ORDER_EVENTS
-SELECT orderid, name, items, cost, ordertime, 'WEBSITE'
+SELECT orderid, name, items, cost, ordertime, 'WEBSITE' AS PURCHASE_METHOD
 FROM ORDER_EVENTS;
 
 INSERT INTO ALL_ORDER_EVENTS
-SELECT CAST(id AS STRING), name, items, price, FROM_UNIXTIME(ordertime), 'APP'
+SELECT CAST(id AS STRING), name, items, price, FROM_UNIXTIME(ordertime), 'APP' AS PURCHASE_METHOD
 FROM ORDER_EVENTS;
