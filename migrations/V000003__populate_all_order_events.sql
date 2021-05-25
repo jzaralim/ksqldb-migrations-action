6f68b767b3380259f5741d90cf592c3e3d7a3700
Zara Lim <zlim@confluent.io>
2021-05-25 13:29:19 -0700

    use correct backticks

--- migrations/V000003__populate_all_order_events.sql
+++ migrations/V000003__populate_all_order_events.sql
@@ -3,5 +3,5 @@ SELECT orderid, name, items, cost, ordertime, ‘WEBSITE’
 FROM ORDER_EVENTS;
 
 INSERT INTO ALL_ORDER_EVENTS
-SELECT CAST(id AS STRING), name, items, price, FROM_UNIXTIME(ordertime), ‘ APP’
+SELECT CAST(id AS STRING), name, items, price, FROM_UNIXTIME(ordertime), ‘APP’
 FROM ORDER_EVENTS;
